72f62e7cf3c2982ecc8f8a1c2e9ea18e04e44e0f
SixSt06 <sixstrokeroll6@gmail.com>
2024-02-26 12:34:50 -0600

    Menu principal y nueva transaccion implementado

--- bin/proyecto_dart_multiplataforma.dart
+++ bin/proyecto_dart_multiplataforma.dart
@@ -9,22 +9,55 @@ void main() {
 
   Transaccion transaccion = Transaccion(monto, concepto, categoria, tipo);
 
-  try {
-    stdout.write('Ingrese el monto: ');
-    monto = double.parse(stdin.readLineSync()!);
+  bool salir = false;
 
-    stdout.write('Ingrese el categoria: ');
-    categoria = stdin.readLineSync()!;
+  while (!salir) {
+    print("");
+    print("!!! Sistema de gestion de finanzas personales !!!");
+    print('1.- Registrar Transacción');
+    print('2.- Ver Saldo y Estado Financiero');
+    print('3.- Establecer Metas y Presupuestos');
+    print('4.- Salir');
+    stdout.write("Ingrese el numero de la opcion a utilizar: ");
+    String opcion = stdin.readLineSync() ?? '';
 
-    stdout.write('Ingrese el concepto: ');
-    concepto = stdin.readLineSync()!;
+    switch (opcion) {
+      case "1":
+        try {
+          stdout.write('Ingrese el monto: ');
+          monto = double.parse(stdin.readLineSync()!);
 
-    stdout.write('Ingrese el tipo (Ingreso o Retiro): ');
-    tipo = stdin.readLineSync()!;
+          stdout.write('Ingrese el categoria: ');
+          categoria = stdin.readLineSync()!;
 
-    transaccion.agregarTransaccion(transaccion);
-    stdout.write("Registro Exitoso!");
-  } catch (e) {
-    print("Error: Ingrese un monto valido!");
+          stdout.write('Ingrese el concepto: ');
+          concepto = stdin.readLineSync()!;
+
+          stdout.write('Ingrese el tipo (Ingreso o Retiro): ');
+          tipo = stdin.readLineSync()!;
+
+          transaccion.agregarTransaccion(transaccion);
+          stdout.write("Transaccion Exitosa!");
+        } catch (e) {
+          print("Error: Ingrese un monto valido!");
+        }
+        break;
+
+      case "2":
+        //generar resumen;
+        break;
+
+      case "3":
+        break;
+
+      case "4":
+        print("Hasta Luego");
+        salir = true;
+        break;
+
+      default:
+        print("Opcion invalida, ingrese una opcion del menú");
+        break;
+    }
   }
 }
