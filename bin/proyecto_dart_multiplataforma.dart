99d61896750d19d27eded477d1aeed46db2bf34d
Biuger <biuger277@gmail.com>
2024-02-27 01:08:50 -0600

    Se agregó el calculo de saldo, gastos y resumen financiero

--- bin/proyecto_dart_multiplataforma.dart
+++ bin/proyecto_dart_multiplataforma.dart
@@ -7,13 +7,15 @@ void main() {
   String concepto = "";
   String tipo = "";
 
+  double saldoActual = 0.0;
+  double ingresos = 0.0;
+  double gastos = 0.0;
+
   Transaccion transaccion = Transaccion(monto, concepto, categoria, tipo);
 
   bool salir = false;
-
+  print("\n!!! Sistema de gestion de finanzas personales !!!");
   while (!salir) {
-    print("");
-    print("!!! Sistema de gestion de finanzas personales !!!");
     print('1.- Registrar Transacción');
     print('2.- Ver Saldo y Estado Financiero');
     print('3.- Establecer Metas y Presupuestos');
@@ -36,27 +38,71 @@ void main() {
           stdout.write('Ingrese el tipo (Ingreso o Retiro): ');
           tipo = stdin.readLineSync()!;
 
-          transaccion.agregarTransaccion(transaccion);
-          stdout.write("Transaccion Exitosa!");
+          if (tipo.toLowerCase() == "ingreso") {
+            transaccion.agregarTransaccion(
+                Transaccion(monto, concepto, categoria, tipo));
+            saldoActual += monto;
+            ingresos += monto;
+            print("\nTransaccion Exitosa!\n");
+          } else if (tipo.toLowerCase() == "retiro") {
+            if (monto > saldoActual) {
+              print("\nNo hay saldo suficiente para el retiro.\n");
+            } else {
+              saldoActual -= monto;
+              gastos += monto;
+              transaccion
+                  .agregarRetiro(Transaccion(monto, concepto, categoria, tipo));
+              print(
+                  "\nSe retiró \$$monto exitosamente con un Concepto de: $concepto\n");
+            }
+          } else {
+            print(
+                "Tipo de transacción desconocida, debe seleccionar entre ingreso o retiro");
+          }
         } catch (e) {
-          print("Error: Ingrese un monto valido!");
+          print("\nError: Ingrese un monto valido!\n");
         }
         break;
 
       case "2":
-        //generar resumen;
+        if (transaccion.transacciones.isEmpty && transaccion.retiros.isEmpty) {
+          print("\nNo hay transacciones realizadas en su cuenta.\n");
+        } else {
+          print(
+              "\n============ Transacciones relaizadas (Ingresos) ==============");
+
+          for (Transaccion transaccion in transaccion.transacciones) {
+            print(
+                "Ingreso de \$${transaccion.monto} con categoría de ${transaccion.categoria} y un concepto de ${transaccion.concepto}");
+          }
+
+          print(
+              "\n============ Transacciones relaizadas (Retiros) ==============");
+          if (transaccion.retiros.isEmpty) {
+            print("No hay retiros en su cuenta. Sigue ahorrando!");
+          } else {
+            for (Transaccion retiro in transaccion.retiros) {
+              print(
+                  "Retiro de \$${retiro.monto}, con categoría de ${retiro.categoria} y un concepto de ${retiro.concepto}");
+            }
+          }
+
+          print("\nIngresos totales: \$$ingresos");
+          print("Gastos totales: \$$gastos");
+          print("Saldo actual: \$$saldoActual\n");
+        }
         break;
 
       case "3":
         break;
 
       case "4":
-        print("Hasta Luego");
+        print("Hasta Luego!");
         salir = true;
         break;
 
       default:
-        print("Opcion invalida, ingrese una opcion del menú");
+        print("\nOpcion invalida, ingrese una opcion del menú\n");
         break;
     }
   }
--- bin/transaccion.dart
+++ bin/transaccion.dart
@@ -7,8 +7,25 @@ class Transaccion {
   Transaccion(this.monto, this.concepto, this.categoria, this.tipo);
 
   List<Transaccion> transacciones = [];
+  List<Transaccion> retiros = [];
 
   void agregarTransaccion(Transaccion transaccion) {
     transacciones.add(transaccion);
+  }
+
+  void agregarRetiro(Transaccion transaccion) {
+    retiros.add(transaccion);
+  }
+}
+
+class Metas {
+  double monto;
+  String concepto;
+  Metas(this.monto, this.concepto);
+
+  List<Metas> metas = [];
+
+  void agregarMeta(Metas meta) {
+    metas.add(meta);
   }
 }
